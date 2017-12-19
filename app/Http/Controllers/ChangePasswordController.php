c548a177d5fe3a02b0d74f228d5036c10a4979ac
celine.duclos <celine.duclos@SIO-SLAM-9.BtsSio-Carriat.com>
2017-12-19 16:12:20 +0100

    MODIF 19/12 celine 16:12

--- app/Http/Controllers/ChangePasswordController.php
+++ app/Http/Controllers/ChangePasswordController.php
@@ -12,7 +12,8 @@ class ChangePasswordController extends Controller
     public function afficheformModifMdp(){
         
         $erreur = "";
-        return view('formModMDP',compact('erreur'));
+        $message = "";
+        return view('formModMDP',compact('erreur', 'message'));
     }
     public function verifMdp(Request $request){
         // Recup ancien mdp 
@@ -23,34 +24,38 @@ class ChangePasswordController extends Controller
         
         // Verif que mdp saisi = ancien mdp
         $bonPwd = $pwd->verifMdp($login, $mdp);
+        $message = "";
         if(empty($bonPwd)){
             $erreur = "Erreur de mot de passe";
-            return view('formModMDP',compact('erreur'));
+            return view('formModMDP',compact('erreur', 'message'));
         }else{
-
             $nouveauMdp = $request->input('npwd');
             $confirmMdp = $request->input('rnpwd');
             // Verif si les nouveaux mdp sont identiques 
             if($nouveauMdp == $confirmMdp){
                 // Modifié le mdp dans la BDD
                 if (preg_match('#^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])#', $nouveauMdp)) {
-                    $erreur = 'Mot de passe conforme';
+                    $erreur = '';
+                    $message = "Mise à jour bien effectuée !";
                     $pwd->setNouveauMdp($nouveauMdp,$login);
-                    return redirect()->back()->with('status','Mise à jour effectuée');
+                    return view('formModMDP', compact('erreur', 'message'));
                 }
                 else {
+                    $message = "";
                     $erreur = 'ERREUR : Mot de passe non conforme. (Il faut une majuscule et un chiffre obligatoirement !)';
-                     return view('formModMDP', compact('erreur'));
+                     return view('formModMDP', compact('erreur', 'message'));
                }	
                 
             }else if($nouveauMdp == $mdp){
                 $erreur = "Mot de passe identique à l'ancien";
-                return view('formModMDP', compact('erreur'));
+                $message = "";
+                return view('formModMDP', compact('erreur', 'message'));
             }
             else{
                 // return Mots de passe différents
                 $erreur = "Mot de passe différents";
-                return view('formModMDP',compact('erreur'));
+                $message = "";
+                return view('formModMDP',compact('erreur', 'message'));
             }
             
         }
